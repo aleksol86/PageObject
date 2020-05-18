7afd4aebf379361b4f02ccdf84f269838b2b82da
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-05-18 14:15:04 +0300

    Добавлен ReplenishPage

---
+++ src/test/java/ru/netology/domain/page/ReplenishPage.java
@@ -0,0 +1,21 @@
+package ru.netology.domain.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.domain.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ReplenishPage {
+    private SelenideElement amountField = $("[data-test-id=\"amount\"] input");
+    private SelenideElement fromField = $("[data-test-id=from] input");
+    private SelenideElement toField = $("[data-test-id=to] input");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement cancelButton = $("[data-test-id=action-cancel]");
+
+    public void replenishCard(DataHelper.CardInfo fromCardInfo) {
+        String amountToAddForTest = "500";
+        amountField.setValue(amountToAddForTest);
+        fromField.setValue(fromCardInfo.getCardNumber());
+        transferButton.click();
+    }
+}
